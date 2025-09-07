a51be739f0309a418d95eca7e63d0124599e1f10
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-07 13:51:15 -0400

    Created PersonTest.java

---
+++ test/PersonTest.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTitle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setYOB() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fullName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void formalName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toCSV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toJSON() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toXML() {
+    }
+}
